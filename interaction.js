6667970c856f47735e0afeaed900e1e578646c84
ange-la <angela.lam938@gmail.com>
2021-10-29 22:18:04 -0400

    Initial commit

---
+++ interaction.js
@@ -0,0 +1,10 @@
+/* ADD SOUND ON CLICK FOR HOME PAGE POSTERS */
+
+/* NAVIGATION BAR */
+
+/* Easier carousel @ https://flickity.metafizzy.co */
+$('.main-carousel').flickity({
+  // options
+  cellAlign: 'left',
+  contain: true
+});
