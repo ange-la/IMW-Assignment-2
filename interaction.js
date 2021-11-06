dc98850d09f9e2b149c734adc0350a206e8cc980
ange-la <angela.lam938@gmail.com>
2021-11-06 02:38:07 -0400

    Fixed navbar, fixed + added minor details & made it responsive

--- interaction.js
+++ interaction.js
@@ -1,10 +1,22 @@
-/* ADD SOUND ON CLICK FOR HOME PAGE POSTERS */
-
-/* NAVIGATION BAR */
-
 /* Easier carousel @ https://flickity.metafizzy.co */
 $('.main-carousel').flickity({
   // options
   cellAlign: 'left',
   contain: true
 });
+
+
+
+/* Responsive navbar @ https://www.w3schools.com/howto/howto_js_responsive_navbar_dropdown.asp */
+function myFunction() {
+  var x = document.getElementById("topnav");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
+
+
+
+/* ADD SOUND ON CLICK FOR HOME PAGE POSTERS */
